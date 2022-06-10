430ad22213757a1f3e29a1678f6e2be92c86ea58
osvilla27 <oscaravg27@gmail.com>
2022-06-10 17:38:21 -0500

    iOS implementation

--- CryptoTracker/src/navigation/Navigation.tsx
+++ CryptoTracker/src/navigation/Navigation.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from '../screens/HomeScreen/index';
 import ListCryptoScreen from '../screens/ListCryptoScreen/index';
 
 const Stack = createNativeStackNavigator();
@@ -8,7 +7,6 @@ const Navigation = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="ListCrypto" component={ListCryptoScreen}/>
-      <Stack.Screen name="Home" component={HomeScreen}/>
     </Stack.Navigator>
   );
 };
