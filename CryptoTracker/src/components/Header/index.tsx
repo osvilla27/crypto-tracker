52452c73d76ad6ebe1db83676ed289eff4a87e75
osvilla27 <oscaravg27@gmail.com>
2022-06-13 15:37:20 -0500

    iOS styles changes

--- CryptoTracker/src/components/Header/index.tsx
+++ CryptoTracker/src/components/Header/index.tsx
@@ -1,16 +1,13 @@
-import { SafeAreaView, View, Text } from 'react-native'
-import React from 'react'
-import { Container, Title, ProfilePhoto } from './styles'
+import React from 'react';
+import {Container, Title, ProfilePhoto} from './styles';
 
 const Header = () => {
   return (
-    <SafeAreaView>
-      <Container>
-        <Title>CryptoTracker Pro</Title>
-        <ProfilePhoto source={require('../../assets/photo.jpg')}/>
-      </Container>
-    </SafeAreaView>
-  )
-}
+    <Container>
+      <Title>CryptoTracker Pro</Title>
+      <ProfilePhoto source={require('../../assets/photo.jpg')} />
+    </Container>
+  );
+};
 
-export default Header
+export default Header;
--- CryptoTracker/src/components/Header/styles.tsx
+++ CryptoTracker/src/components/Header/styles.tsx
@@ -1,19 +1,21 @@
 import styled from 'styled-components/native';
+import {Platform} from 'react-native';
 
 export const Container = styled.View`
-  background-color: #385775
-  padding: 30px 20px
   flex-direction: row
   justify-content: space-between
   align-items: center
-`
+  padding: 20px
+  padding-top: ${Platform.OS === 'ios' ? '50px' : '20px'}
+  background-color: #385775
+`;
 export const Title = styled.Text`
   color: white
   font-size: 22px
   font-weight: bold
-`
+`;
 export const ProfilePhoto = styled.Image`
   height: 50px
   width: 50px
   border-radius: 25px
-`
+`;
